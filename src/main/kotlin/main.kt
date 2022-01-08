437c3ac8c0f391aeabae988df6ec3307a1e4c416
Robbie <robbiebowman@gmail.com>
2022-01-07 23:05:28 -0500

    Loop game after successful guess

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,22 +6,24 @@ const val PARTIAL = 'y'
 const val MISS = 'b'
 
 fun main(args: Array<String>) {
-    val corpus = getWords("five-letter-words.txt")
-    var remainingPossibilities = corpus
-    var guessResult: String
-    val guessedWordsAndResults = mutableListOf<Pair<String, String>>()
-    while (remainingPossibilities.count() > 1) {
-        val charUsages = countCharUsages(remainingPossibilities, guessedWordsAndResults)
-        val guess = wordWithHighestScore(corpus) { i -> mostFreqUsage(i, charUsages, guessedWordsAndResults) }
-        println("Try ${guess.uppercase()}")
-        println("Input results (g for green, y for yellows, b for blacks. Example: bbbyg):")
-        guessResult = readLine()!!
-        guessedWordsAndResults.add(Pair(guess, guessResult))
-        remainingPossibilities = buildNewWords(remainingPossibilities, guess, guessResult)
-        println("${remainingPossibilities.size} possibilities remain. ")
-        if (remainingPossibilities.size < 20) println("Those are: $remainingPossibilities")
+    while (true) {
+        val corpus = getWords("five-letter-words.txt")
+        var remainingPossibilities = corpus
+        var guessResult: String
+        val guessedWordsAndResults = mutableListOf<Pair<String, String>>()
+        while (remainingPossibilities.count() > 1) {
+            val charUsages = countCharUsages(remainingPossibilities, guessedWordsAndResults)
+            val guess = wordWithHighestScore(corpus) { i -> mostFreqUsage(i, charUsages, guessedWordsAndResults) }
+            println("Try ${guess.uppercase()}")
+            println("Input results (g for green, y for yellows, b for blacks. Example: bbbyg):")
+            guessResult = readLine()!!
+            guessedWordsAndResults.add(Pair(guess, guessResult))
+            remainingPossibilities = buildNewWords(remainingPossibilities, guess, guessResult)
+            println("${remainingPossibilities.size} possibilities remain. ")
+            if (remainingPossibilities.size < 20) println("Those are: $remainingPossibilities")
+        }
+        println("Hooray! The answer is ${remainingPossibilities.first().uppercase()}")
     }
-    println("Hooray! The answer is ${remainingPossibilities.first().uppercase()}")
 }
 
 fun buildNewWords(currentWords: List<String>, guess: String, result: String): List<String> {
